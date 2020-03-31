3e636950cc671ec79bd2a237b124eed45088ae58
Renato França <renato@frannca.com>
2020-03-31 10:01:36 -0300

    feat: login page

--- src/app/modules/authentication/authentication.module.ts
+++ src/app/modules/authentication/authentication.module.ts
@@ -4,6 +4,12 @@ import { CommonModule } from '@angular/common';
 import { AuthenticationRoutingModule } from './authentication-routing.module';
 import { LoginPage } from 'src/app/pages/admin/login/login.page';
 import { AuthenticationTemplate } from 'src/app/templates/admin/authentication/authentication.template';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
@@ -13,7 +19,13 @@ import { AuthenticationTemplate } from 'src/app/templates/admin/authentication/a
   ],
   imports: [
     CommonModule,
-    AuthenticationRoutingModule
+    AuthenticationRoutingModule,
+    FlexLayoutModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule
   ]
 })
 export class AuthenticationModule { }
--- src/app/pages/admin/login/login.page.ts
+++ src/app/pages/admin/login/login.page.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss']
 })
 export class LoginPage implements OnInit {
+  hidePassword = true;
+  passwordInputType = 'password';
+  passwordIconType = 'visibility_off';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  togglePasswordVisibility() {
+    this.hidePassword = !this.hidePassword;
+
+    this.passwordInputType = this.hidePassword ? 'password' : 'text';
+    this.passwordIconType = this.hidePassword ? 'visibility_off' : 'visibility';
   }
 
 }
